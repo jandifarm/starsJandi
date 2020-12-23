1ce434c9b77fc7cab695aa70d01a8a63d22ede7e
lebcoco <leee24638@gmail.com>
2020-12-23 11:44:50 +0900

    Upload Spring Boot Study Folder

---
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,8 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+
+}
---
+++ hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -0,0 +1,9 @@
+package hello.hellospring.domain;
+
+public class Member {
+
+  //시스템이 지정한 id
+  private long id;
+  //고객이 지정한 id
+  private String name;
+}
