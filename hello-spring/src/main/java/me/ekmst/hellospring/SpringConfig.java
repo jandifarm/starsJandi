d4ef51ccadc105bf2b1b7878059996120d952077
lebcoco <leee24638@gmail.com>
2020-12-24 15:28:35 +0900

    Upload error

---
+++ hello-spring/src/main/java/me/ekmst/hellospring/SpringConfig.java
@@ -0,0 +1,34 @@
+package me.ekmst.hellospring;
+
+import me.ekmst.hellospring.repository.*;
+import me.ekmst.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+
+    private final MemberRepository memberRepository;
+
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
+
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JpaMemberRepository(em);
+//    }
+
+}
