35c4879e0921fe65d092be7d537b5de6c5096302
lebcoco <leee24638@gmail.com>
2020-12-24 15:30:15 +0900

    Upload

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -1,36 +1,29 @@
 package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-@Service
 public class MemberService {
-  private final MemberRepository memberRepository;
+  private final MemberRepository memberRepository = new MemoryMemberRepository();
 
-  @Autowired
-  public MemberService(MemberRepository memberRepository){
-    this.memberRepository = memberRepository;
+  public MemberService(MemoryMemberRepository memberRepository) {
   }
+
   /**
    * 회원가입
    */
   public Long join(Member member) {
-
-    validateDuplicateMember(member); // 중복 회원 검증
+    validateDuplicateMember(member); //중복 회원 검증
     memberRepository.save(member);
     return member.getId();
   }
-
   private void validateDuplicateMember(Member member) {
     memberRepository.findByName(member.getName())
         .ifPresent(m -> {
           throw new IllegalStateException("이미 존재하는 회원입니다.");
         });
   }
-
   /**
    * 전체 회원 조회
    */
@@ -41,5 +34,4 @@ public class MemberService {
   public Optional<Member> findOne(Long memberId) {
     return memberRepository.findById(memberId);
   }
-
 }
